ed7db490b091738b5ec8d28350b5e7193ee41448
Ayawow <1123325454@qq.com>
2023-07-23 11:42:46 +0800

    图片懒加载

--- src/main.js
+++ src/main.js
@@ -7,10 +7,25 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { useIntersectionObserver } from '@vueuse/core'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.directive('img-lazy', {
+    mounted(el, binding) {
+        useIntersectionObserver(
+            el,
+            ([{ isIntersecting }]) => {
+
+                if (isIntersecting) {
+                    el.src = binding.value
+                }
+            }
+        )
+    }
+})
 
 app.mount('#app')
