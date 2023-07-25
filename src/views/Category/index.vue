6bb897849ae6501b6e578d1ef7aa3350ea4f9a37
Ayawow <1123325454@qq.com>
2023-07-25 17:50:01 +0800

    二级分类-整体认识和路由配置

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -32,7 +32,7 @@
             <h3>全部分类</h3>
             <ul>
                 <li v-for="i in categoryData.children" :key="i.id">
-                    <RouterLink to="/">
+                    <RouterLink :to="`sub/${i.id}`">
                         <img v-img-lazy="i.picture" />
                         <p>{{ i.name }}</p>
                     </RouterLink>
