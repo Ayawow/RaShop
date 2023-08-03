ebd8f17297a66b0fd6ffc5b28ba5d2ace5790239
Ayawow <1123325454@qq.com>
2023-08-03 11:14:49 +0800

    登录-表单验证

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,5 +1,37 @@
 <script setup>
 
+    import { ref } from 'vue'
+
+    //表单对象
+    const form = ref({
+        account: '',
+        password: ''
+    })
+
+    //规则对象
+    const rules = {
+        account: [
+            {
+                required: true, message: '用户不能为空', trigger: 'blur'
+            }
+        ],
+        password: [
+            {
+                required: true, message: '密码不能为空', trigger: 'blur'
+            },
+            {
+                min: 6, max: 14, message: '密码最小6位数，最大14位数', trigger: 'blur'
+            }
+        ],
+        agree: [
+            {
+                validator: (rule, val, callback) => {
+                    return val ? callback() : new Error('请先同意协议')
+                }
+            }
+        ]
+    }
+
 </script>
 
 
@@ -24,15 +56,15 @@
                 </nav>
                 <div class="account-box">
                     <div class="form">
-                        <el-form label-position="right" label-width="60px" status-icon>
-                            <el-form-item label="账户">
-                                <el-input />
+                        <el-form :model="form" :rules="rules" label-position="right" label-width="60px" status-icon>
+                            <el-form-item prop="account" label="账户">
+                                <el-input v-model="form.account" />
                             </el-form-item>
-                            <el-form-item label="密码">
-                                <el-input />
+                            <el-form-item prop="password" label="密码">
+                                <el-input v-model="form.password" />
                             </el-form-item>
-                            <el-form-item label-width="22px">
-                                <el-checkbox size="large">
+                            <el-form-item prop=agree label-width="22px">
+                                <el-checkbox v-model="form.agree" size="large">
                                     我已同意隐私条款和服务条款
                                 </el-checkbox>
                             </el-form-item>
