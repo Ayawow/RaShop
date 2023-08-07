50590022bbcb76c810989f6718ca9eddad743bc1
Ayawow <1123325454@qq.com>
2023-08-07 16:15:50 +0800

    头部购物车-计算逻辑

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -1,7 +1,7 @@
 // 封装购物车模块
 
 import { defineStore } from 'pinia'
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 
 
 export const useCartStore = defineStore('cart', () => {
@@ -33,10 +33,16 @@ export const useCartStore = defineStore('cart', () => {
         cartList.value.splice(idx, 1)
     }
 
+    //计算
+    const allCount = computed(() => cartList.value.reduce((a, c) => a + c.count, 0))
+
+    const allPrice = computed(() => cartList.value.reduce((a, c) => a + c.count * c.price, 0))
     return {
         cartList,
         addCart,
-        delCart
+        delCart,
+        allCount,
+        allPrice
     }
 }, {
     persist: true,
